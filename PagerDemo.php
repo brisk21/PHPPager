743a2bba3d5ed02cb594ff7a882a9c5cf16ce1b2
Brisk Ver <1076963452@qq.com>
2019-07-03 14:39:01 +0800

    Update PagerDemo.php

--- PagerDemo.php
+++ PagerDemo.php
@@ -1,6 +1,8 @@
 <head>   
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />   
 <title>----分页演示-----</title>   
+	
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>   
 <body>   
     <?php   
